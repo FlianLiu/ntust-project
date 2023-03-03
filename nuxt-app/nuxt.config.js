6db97650569b9e06ff28958312dfdef1509a49eb
Flian <56669890+FlianLiu@users.noreply.github.com>
2023-03-03 20:06:36 +0800

    cancel deploy to github pages v1.2

--- nuxt-app/nuxt.config.js
+++ nuxt-app/nuxt.config.js
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/ntust-project/'
-  },
   vue: {
     config: {
       productionTip: false,
